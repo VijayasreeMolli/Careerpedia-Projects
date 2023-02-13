35ba2d4c7fb1ccd3f1b6081a07adcd511749720e
Vijayasree Molli <vijayasree.palla@gmail.com>
2023-02-13 16:15:07 +0530

    first commit

---
+++ JavaScript/app.js
@@ -0,0 +1,53 @@
+
+
+
+// console.log("Welcom to My Web Page")
+// console.log("Welcom to My Web Page")
+// console.log("Welcom to My Web Page")
+// console.log("Welcom to My Web Page")
+
+
+// for (let i = 0; i <= 2;i++ ) {
+//     console.log("Welcom to My Web Page");
+// }
+
+
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+for(let i=10;i>0;i--){
+    console.log(i);
+}
+
+// // console.log("Hello World!");
+
+// // console.log("This is my first External Java Script");
+
+// // var a="Hello";
+// // console.log(a);
+
+// // const b= 60;
+// // const c= 40;
+// // console.log(b+c);
+
+// var number = 12345 +"80"; //Number Data type
+
+// console.log(number)
+
+// let name = "Vijayasree"; // String Data type
+
+// console.log(name)
+
+// const $text = true; //Boolean Data type
+
+// console.log($text)
+
+// const id = null; //Null Data type
+
+// console.log(id)
+
+// var fullname; //Undefined Date type
+
+// console.log(fullname)
+
